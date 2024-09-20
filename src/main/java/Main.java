e9794780d005a3261ba88f0b53c3bb0ee66debda
Daniel-cyber11 <daniel.gilboa3@gmail.com>
2024-09-20 08:58:23 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+
+public class Main {
+	    public static void main(String[] args) {
+	        BamkAccount acct1 = new BamkAccount(3000,5789);
+	        System.out.println(acct1.getBalance());	 
+	        System.out.println("bal"+acct1.setBalance(3)); 
+	        acct1.deposit(300);}
+	    
+	}
+
+
+
+
+
